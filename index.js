7add2589ffc506b8214c22213e35bfe96add9570
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-05 22:31:36 +0300

    declare variables and assign them array values

---
+++ index.js
@@ -0,0 +1,11 @@
+var maleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var femaleNames =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+function akanNaming (){
+    var dateborn = document.getElementById("dateborn").value;
+    var dd = parseInt(dateborn);
+    var monthborn = document.getElementById("monthborn").value;
+
+
+
+
+}
