a768f256d5f1615be1c6cce2b79826d5bd99c96c
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-06 22:17:17 +0300

    restructure code order and add clearInputs function

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-var maleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-var femaleNames =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+const maleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+const femaleNames =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+const days =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const displayMessage = document.getElementById('displaymessage') ;
 function akanNaming(){
     var dateborn = document.getElementById("dateborn").value;
-    var dd = parseInt(dateborn);
     var monthborn = document.getElementById("monthborn").value;
-    var mm = parseInt(monthborn);
     var yearborn = document.getElementById("yearborn").value;
+    var gender=document.getElementById("gender").value;
+    if (dateborn<=0||dateborn>31||dateborn ===""){
+        alert('Your date is invalid');
+        return;
+    }
+    else if(monthborn<1 ||monthborn>12 ||(monthborn==2 && dateborn>29) ||monthborn===""){
+        alert( 'Your month is invalid');
+        return;
+    }
+    else if (yearborn<=0 ||yearborn==""){
+        alert('Your year of birth is invalid')
+        return;
+    }
+    var dd = parseInt(dateborn);
+    var mm = parseInt(monthborn);
     var yy = parseInt(yearborn.substr(2,4));
     var cc = parseInt(yearborn.substr(0,2));
     var dayborn = parseInt(((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10))+ dd)%7;
-    var gender=document.getElementById("gender").value;
-    if (dd<=0||dd>31||dd ==""){
-        alert('Your date is invalid');
+     console.log({dd,mm,yearborn,yy,cc,dayborn,gender,});
+    
+    if(gender.toLowerCase()==="male"){
+        displayMessage.innerHTML ='Your day of birth is ' + days[dayborn] +' and your Akan name is '+ maleNames[dayborn];
+        clearInputs();
+        // alert('Your day of birth is' + days[dayborn] +' and your Akan name is '+ maleNames[dayborn]);
     }
-    else if(mm<1 ||mm>12 ||mm==2 && dd>29 ||mm==""){
-        alert( 'Your month is invalid');
+    else if( gender.toLowerCase()==='female'){
+        displayMessage.innerHTML ='Your day of birth is ' + days[dayborn] +' and your Akan name is '+ femaleNames[dayborn];
+        clearInputs();
+        // alert('Your day of birth is' + days[dayborn] +' and your Akan name is '+ femaleNames[dayborn]);
     }
-    else if (year<=0 ||year==""){
-        alert('Your year of birth is invalid')
-    }
-    if(gender==="male"){
-        alert('Your day of birth is' + dayborn +' and your Akan name is '+ maleName[day]);
-    }
-    else if( gender==='female'){
-        alert('Your day of birth is' + dayborn +' and your Akan name is '+ femaleName[day]);
-    }
+}
+const clearInputs = ()=>{
+        document.getElementById("dateborn").value ='';
+        document.getElementById("monthborn").value ='';
+        document.getElementById("yearborn").value ='';
+        document.getElementById("gender").value =''; 
 }
